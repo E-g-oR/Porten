6c4a413f19954138994142da5f3add2af7d708c8
Egor <68065928+E-g-oR@users.noreply.github.com>
2020-11-14 15:21:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const SubMenuButton = document.querySelector('.sub-menu-button'),
+  Menu = document.querySelector('.menu');
+var MatchMedia = window.matchMedia('all and (max-width: 570px)');
+
+const Top = () => {
+  return `
+  <div class="top">
+    <div class="container">
+      <div class="top__body">
+        <div class="top__info info">
+          <div class="info__phone">
+            <a href="#">
+              <!-- <img src="img/icons/phone.svg" alt="">  -->
+              8 (812) 123-45-67
+            </a>
+          </div>
+          <span class="info__week">Работаем 7 дней в неделю</span>
+          <span class="info__time">9:00 — 18:00</span>
+        </div>
+        <a class="top__login" href="#">Войти/Регистрация</a>
+      </div>
+    </div>
+  </div>
+`
+}
+if (MatchMedia) {
+  Menu.insertAdjacentHTML('beforeend', Top());
+}
+
+SubMenuButton.addEventListener('click', () => {
+  SubMenuButton.classList.toggle('_menu_active');
+  Menu.classList.toggle('_menu_active');
+})
