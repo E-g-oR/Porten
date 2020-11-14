7858186626d48cd9d2df649f1c1c38772aef16f1
Egor <68065928+E-g-oR@users.noreply.github.com>
2020-11-14 18:57:03 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,12 +26,11 @@ const Top = () => {
 
 
 SubMenuButton.addEventListener('click', () => {
-  SubMenuButton.classList.toggle('_menu_active');
-  Menu.classList.toggle('_menu_active');
   if (!Menu.lastElementChild.classList.contains('top') && window.innerWidth <= 570) {
     Menu.insertAdjacentHTML('beforeend', Top());
   } else if (Menu.lastElementChild.classList.contains('top') && window.innerWidth > 570) {
     Menu.lastChild.remove();
   }
-
+  SubMenuButton.classList.toggle('_menu_active');
+  Menu.classList.toggle('_menu_active');
 })
